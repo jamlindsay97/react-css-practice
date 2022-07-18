fc486448d6ff6b7afcd407dadf3192d3d20fffe6
James Lindsay <jamlindsay97@gmail.com>
2022-07-18 16:26:54 +0100

    done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import "./App.css";
+import { TheApp } from "./styles/app.styled.js";
+
+function App() {
+  return (
+    <TheApp>
+      <h1>Hello world</h1>
+    </TheApp>
+  );
+}
+
+export default App;
---
+++ src/styles/app.styled.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const TheApp = styled.div`
+  text-align: center;
+`;

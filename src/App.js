187f6237766432c98dd271b2f4d79de927fab707
James Lindsay <jamlindsay97@gmail.com>
2022-07-19 10:48:44 +0100

    browser-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import "./App.css";
 import { TheApp } from "./styles/app.styled.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home.js";
+import About from "./pages/about.js";
 
 function App() {
   return (
     <TheApp>
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
-          <Route path="/about" />
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}/>
         </Routes>
       </BrowserRouter>
     </TheApp>

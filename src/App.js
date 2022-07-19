8e3e17d7ac14207e7642720c50f9044f6f85c612
James Lindsay <jamlindsay97@gmail.com>
2022-07-19 15:20:54 +0100

    james routes and css update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 // import "./App.css";
 import { TheApp } from "./styles/app.styled.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar.js";
 import Home from "./pages/home.js";
 import About from "./pages/about.js";
+import Contact from "./pages/contact.js";
+import Profile from "./pages/profile.js";
 
 function App() {
   return (
     <TheApp>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </TheApp>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,32 @@
 import { Link } from "react-router-dom";
+import { TheNavbar, NavItem } from "../styles/navbar.styled.js"
+
 
 const Navbar = () => {
+  const navbarList = ["Home", "About", "Contact", "Profile"];
   return (
-    <div>
-      <Link to="/">
-        <p>Home</p>
-      </Link>
-      <Link to="/about">
-        <p>About</p>
-      </Link>
-    </div>
+    //where the flexbox is
+    <TheNavbar>
+      {navbarList.map((navbarItem) => {
+        // /home doesnt exist 
+        // /About
+        let path = "/";
+        if (navbarItem !== "Home") {
+          path = "/" + navbarItem;
+        }
+        return (
+          <Link to={path}>
+            <NavItem>{navbarItem}</NavItem>
+          </Link>
+        );
+      })}
+    </TheNavbar>
   );
 };
 
 export default Navbar;
+
+
+// {optionButtons.map((optionButton) => {
+//   return <button>{optionButton.Status}</button>;
+// })}

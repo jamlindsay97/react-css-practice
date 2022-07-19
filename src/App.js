b8343d7dfc26de60028784296458279530116a09
Indykatz <cats83@me.com>
2022-07-19 11:33:43 +0100

    Kat linked nav for ya

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { TheApp } from "./styles/app.styled.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home.js";
 import About from "./pages/about.js";
+import Navbar from "./components/navbar.js";
 
 function App() {
   return (
     <TheApp>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </TheApp>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const navbarList = ["Home", "About"];
   return (
     <div>
-      <Link to="/">
-        <p>Home</p>
-      </Link>
-      <Link to="/about">
-        <p>About</p>
-      </Link>
+      {navbarList.map((navbarItem) => {
+        // /home doesnt exist 
+        // /About
+        let path = "/";
+        if (navbarItem !== "Home") {
+          path = "/" + navbarItem;
+        }
+        return (
+          <Link to={path}>
+            <p>{navbarItem}</p>
+          </Link>
+        );
+      })}
     </div>
   );
 };
